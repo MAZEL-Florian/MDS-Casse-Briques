68561f843fc5f7cbf8c1703d0e32a530558f4686
MAZEL Florian <mazel.florian1@gmail.com>
2025-03-10 16:17:03 +0100

    feat: added bricks health

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -10,6 +10,8 @@ public class Paddle : MonoBehaviour
 
     public float speed = 30f;
 
+    public float maxBounceAngle = 75f;
+
     private void Awake()
     {
         this.rigidbody = GetComponent<Rigidbody2D>();
@@ -36,6 +38,27 @@ public class Paddle : MonoBehaviour
         if (this.direction != Vector2.zero)
         {
             this.rigidbody.AddForce(this.direction * this.speed);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Ball ball = collision.gameObject.GetComponent<Ball>();
+
+        if (ball != null)
+        {
+            Vector3 paddlePosition = this.transform.position;
+            Vector2 contactPoint = collision.GetContact(0).point;
+
+            float offset = paddlePosition.x - contactPoint.x;
+            float width = collision.otherCollider.bounds.size.x / 2;
+
+            float currentAngle = Vector2.SignedAngle(Vector2.up, ball.rigidbody.linearVelocity);
+            float bounceAngle = (offset / width) * this.maxBounceAngle;
+            float newAngle = Mathf.Clamp(currentAngle + bounceAngle, -this.maxBounceAngle, this.maxBounceAngle);
+
+            Quaternion rotation = Quaternion.AngleAxis(newAngle, Vector3.forward);
+            ball.rigidbody.linearVelocity = rotation * Vector2.up * ball.rigidbody.linearVelocity.magnitude;
         }
     }
 }
